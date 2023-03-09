20639233fe22821a6c25eee5dcbd782787d2844d
jaguilar <julio.aguilar@lania.edu.mx>
2023-03-09 16:41:46 -0600

    Usa servicio para consultar autos

--- src/app/Automoviles/Automoviles.component.ts
+++ src/app/Automoviles/Automoviles.component.ts
@@ -1,9 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Automovil } from "./automovil.model";
+import { AutomovilesService } from "./automoviles.service";
 
 @Component({
   selector:"app-automoviles",
   templateUrl:"./Automoviles.component.html"
 })
-export class Automoviles {
+export class Automoviles implements OnInit {
 
+  listaAutomoviles:Automovil[] = [];
+  constructor(private autoService: AutomovilesService) {}
+
+  ngOnInit(): void {
+    this.listaAutomoviles = this.autoService.getAutomoviles();
+  }
 }
