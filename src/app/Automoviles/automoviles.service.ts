6cf68706db171cb93ba2aa73ffdb8448938502cf
jaguilar <julio.aguilar@lania.edu.mx>
2023-03-09 16:40:44 -0600

    Agrega metodo de consulta

--- src/app/Automoviles/automoviles.service.ts
+++ src/app/Automoviles/automoviles.service.ts
@@ -8,7 +8,20 @@ export class AutomovilesService {
 
   getAutomoviles() : Automovil[] {
     return [
-
+      {
+        placas:"PYQ1839",
+        marca:"AAA",
+        modelo:"AAA 2023",
+        color:"Azul",
+        kilometraje: 230
+      },
+      {
+        placas:"RTQ1839",
+        marca:"BBB",
+        modelo:"BBB 2021",
+        color:"Verde",
+        kilometraje: 172
+      }
     ];
   }
 }
